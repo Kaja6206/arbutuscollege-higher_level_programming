3c637f838724a24c4a352350538a977327cc3778
Kaja6206 <kasobojanic@gmail.com>
2023-08-30 14:08:00 -0700

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,11 @@
--- List all shows and genres linked to show from 'hbtn_0d_tvshows'
--- If show doesn't have a genre, display NULL in genre column
--- Each record should display tv_shows.title, tv_genres.name
--- Results must be sorted in ascending order by show title
--- You can only use one SELECT statement
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+-- Lists all genres from the database hbtn_0d_tvshows along with the number of
+-- shows linked to each.
+-- Does not display genres without linked shows.
+-- Records are ordered by descending number of shows linked.
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
